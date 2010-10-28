c3e2f26f6855811881b101f7714a7efe772672d8
cekk <andrea.cecchi85@gmail.com>
2010-10-28 07:21:21 +0000

    rimosso indice

--- rer/sitesearch/Extensions/install.py
+++ rer/sitesearch/Extensions/install.py
@@ -5,16 +5,6 @@ def install(portal):
     setup_tool.setImportContext('profile-rer.sitesearch:default')
     setup_tool.runAllImportSteps()
 
-    pc = getToolByName(portal, 'portal_catalog')
-    indexes = pc.indexes()
-    my_index = ["searchSubject"]
-    for indice in my_index:
-        if indice in indexes:
-            portal.plone_log("Found the '%s' index in the catalog, nothing changed." %indice)
-        else:
-            pc.addIndex(name=indice,type="KeywordIndex")
-            portal.plone_log("Added '%s' (KeywordIndex) to the catalog." %indice)
-
 def uninstall(portal, reinstall=False):
     if not reinstall:
         setup_tool = portal.portal_setup
