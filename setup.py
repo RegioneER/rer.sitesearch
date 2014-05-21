c2d1cf4741748895288797bd2c380ffdc0597d79
Andrea Cecchi <andrea.cecchi85@gmail.com>
2014-05-21 15:30:41 +0200

    renamed README

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests_require = ['plone.app.testing',
 setup(name='rer.sitesearch',
       version=version,
       description="A product that change the base site search of Plone with some new features.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
