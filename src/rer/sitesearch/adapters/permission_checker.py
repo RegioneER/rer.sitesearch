8877721504baec34987456900b1fd91e0bb8872e
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-11-11 15:17:46 +0100

    fix install

--- src/rer/sitesearch/adapters/permission_checker.py
+++ src/rer/sitesearch/adapters/permission_checker.py
@@ -1,24 +1,9 @@
-from AccessControl import getSecurityManager
-from plone.app.z3cform.interfaces import IPloneFormLayer
-from plone.autoform.interfaces import WIDGETS_KEY
-from plone.autoform.interfaces import WRITE_PERMISSIONS_KEY
-from plone.autoform.utils import resolveDottedName
-from plone.dexterity.interfaces import IDexterityContent
-from plone.dexterity.utils import iterSchemata
-from plone.supermodel.utils import mergedTaggedValueDict
-from z3c.form.interfaces import IFieldWidget
-from z3c.form.interfaces import IForm
+# -*- coding: utf-8 -*-
 from zope.component import adapter
-from zope.component import queryMultiAdapter
-from zope.component import queryUtility
-from zope.deprecation import deprecated
 from zope.interface import implementer
-from zope.publisher.browser import TestRequest
-from zope.security.interfaces import IPermission
 from plone.app.dexterity.permissions import GenericFormFieldPermissionChecker
 from rer.sitesearch.interfaces import IRerSiteSearchSettingsForm
 
-import six
 
 try:
     from plone.app.z3cform.interfaces import IFieldPermissionChecker
