542288d29ce5118bc000a8e2fa65063c5b64c373
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-11-11 12:05:50 +0100

    add buildout cfg

--- setup.py
+++ setup.py
@@ -1,48 +1,65 @@
 from setuptools import setup, find_packages
 import os
 
-version = '3.2.7.dev0'
+version = "3.2.7.dev0"
 
 setup(
-    name='rer.sitesearch',
+    name="rer.sitesearch",
     version=version,
     description="A product that change the base site search of Plone with some new features.",
     long_description=open("README.rst").read()
     + "\n"
-    + open(os.path.join("docs", "HISTORY.rst")).read(),
+    + open("CHANGELOG.rst").read(),
     # Get more strings from
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
         "Framework :: Plone :: Addon",
         "Framework :: Plone",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python",
     ],
-    keywords='',
-    author='RedTurtle Technology',
-    author_email='sviluppoplone@redturtle.net',
-    url='http://svn.plone.org/svn/collective/',
-    license='GPL',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['rer'],
+    keywords="",
+    author="RedTurtle Technology",
+    author_email="sviluppo@redturtle.it",
+    url="https://github.com/RegioneER/rer.sitesearch",
+    project_urls={
+        "PyPI": "https://pypi.python.org/pypi/rer.sitesearch",
+        "Source": "https://github.com/RegioneER/rer.sitesearch",
+        "Tracker": "https://github.com/RegioneER/rer.sitesearch/issues",
+    },
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["rer"],
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
+    python_requires="==2.7",
+    install_requires=[
+        "setuptools",
+        # -*- Extra requirements: -*-
+        "z3c.jbot",
+        "plone.api>=1.8.4",
+        "plone.restapi",
+        "plone.app.dexterity",
+    ],
     extras_require={
-        'test': [
-            'plone.app.testing',
+        "test": [
+            "plone.app.testing",
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
+            "plone.testing>=5.0.0",
+            "plone.app.contenttypes",
+            "plone.app.robotframework[debug]",
         ]
     },
-    install_requires=['setuptools', 'plone.api'],
     entry_points="""
-      # -*- entry_points -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
+    [z3c.autoinclude.plugin]
+    target = plone
+    [console_scripts]
+    update_locale = rer.search.locales.update:update_locale
+    """,
 )
